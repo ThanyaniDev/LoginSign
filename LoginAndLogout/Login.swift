3bec130c0c16a2a546b0e4faab67e7c440ac8654
ThanyaniTshikonwani <thanyani.tshikonwani@umuzi.org>
2020-09-03 08:39:44 +0200

    first commit

---
+++ LoginAndLogout/Login.swift
@@ -0,0 +1,21 @@
+//
+//  Login.swift
+//  LoginAndLogout
+//
+//  Created by Thanyani on 2020/09/03.
+//  Copyright © 2020 Thanyani. All rights reserved.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Login_Previews: PreviewProvider {
+    static var previews: some View {
+        Login()
+    }
+}
---
+++ LoginAndLogout/SignOut.swift
@@ -0,0 +1,21 @@
+//
+//  SignOut.swift
+//  LoginAndLogout
+//
+//  Created by Thanyani on 2020/09/03.
+//  Copyright © 2020 Thanyani. All rights reserved.
+//
+
+import SwiftUI
+
+struct SignOut: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignOut_Previews: PreviewProvider {
+    static var previews: some View {
+        SignOut()
+    }
+}
